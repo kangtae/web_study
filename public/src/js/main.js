f0968bf4176a6c6696b5f7d639228015d2d2c1f0
boribaram <bras1717@naver.com>
2020-10-11 17:14:18 +0900

    메인작업중333

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     $('.js-celebrity1').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots:false,
+        arrows:false
     });
 });
