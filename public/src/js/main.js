91c3197f928ef0bfd25c641f31072434cdf24ba4
boribaram <bras1717@naver.com>
2020-10-26 23:43:16 +0900

    메인 다시 작업중

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -3,8 +3,17 @@ $(document).ready(function(){
     $('.js-celebrity1').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         dots:false,
-        arrows:false
+        arrows:false,
+        responsive: [
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }
+        ]
     });
 });
