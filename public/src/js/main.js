a6c6420d6fbde889245dd1ec32fed97fdfb80e8b
boribaram <bras1717@naver.com>
2020-10-28 23:25:36 +0900

    pc 작업완료

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -16,4 +16,60 @@ $(document).ready(function(){
             }
         ]
     });
+
+    //collection
+    var collectionNum = 0;
+    var collectionTotal = $('.collection-img__item').length -1;
+    $('.js-collection-next').on('click',function(e){
+        e.preventDefault();
+        if (collectionNum < collectionTotal){
+            collectionNum++;
+        }else{
+            collectionNum = 0;
+        }
+        $('.collection-img__item').removeClass('is-past').each(function () {
+            var itemClass = $(this).attr('class');
+            $(this).attr('class', itemClass.replace('is-active', 'is-past'));
+        });
+        $('.collection-txt__item').removeClass('is-active').eq(collectionNum).addClass('is-active');
+        $('.collection-img__item').removeClass('is-active').eq(collectionNum).addClass('is-active');
+        $('.collection-nav__item').removeClass('is-active').eq(collectionNum).addClass('is-active');
+        setTimeout(function(){
+            $('.collection-img__item').removeClass('is-past')
+        },500)       
+    });
+
+    $('.js-collection-prev').on('click', function (e) {
+        e.preventDefault();
+        if (collectionNum > 0) {
+            collectionNum--;
+        } else {
+            collectionNum = collectionTotal;
+        }
+        $('.collection-img__item').removeClass('is-past').each(function () {
+            var itemClass = $(this).attr('class');
+            $(this).attr('class', itemClass.replace('is-active', 'is-past'));
+        });
+        $('.collection-txt__item').removeClass('is-active').eq(collectionNum).addClass('is-active');
+        $('.collection-img__item').removeClass('is-active').eq(collectionNum).addClass('is-active');
+        $('.collection-nav__item').removeClass('is-active').eq(collectionNum).addClass('is-active');   
+        setTimeout(function () {
+            $('.collection-img__item').removeClass('is-past')
+        }, 500) 
+    });
+
+    $('.collection-nav__item').on('click',function(e){
+        e.preventDefault();
+        collectionNum = $(this).index();
+        $('.collection-img__item').removeClass('is-past').each(function () {
+            var itemClass = $(this).attr('class');
+            $(this).attr('class', itemClass.replace('is-active', 'is-past'));
+        });
+        $('.collection-txt__item').removeClass('is-active').eq(collectionNum).addClass('is-active');
+        $('.collection-img__item').removeClass('is-active').eq(collectionNum).addClass('is-active');
+        $('.collection-nav__item').removeClass('is-active').eq(collectionNum).addClass('is-active');
+        setTimeout(function () {
+            $('.collection-img__item').removeClass('is-past')
+        }, 500)
+    });
 });
--- public/src/js/style.js
+++ public/src/js/style.js
@@ -1,22 +1,35 @@
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+        $(".header, .right-nav").addClass("is-active");
+    } else {
+        $(".header, .right-nav").removeClass("is-active");
+    }
+});
+
 $('.js-gnb-btn').on('click',function(e){
     e.preventDefault();
     if($(this).hasClass('is-active')){
         $(this).removeClass('is-active');
         $('.gnb').removeClass('is-active');
+        $('.right-nav').removeClass("type-white");
     }else{
         $(this).addClass('is-active');
         $('.gnb').addClass('is-active');
+        $('.right-nav').addClass("type-white");
     }
     
 });
 
 $('.gnb-depth1__link').on('click',function(e){
     e.preventDefault();
+    var depth1 = $(this).parents('.gnb-depth1__item')
     var depth2 = $(this).parents('.gnb-depth1__item').find('.gnb-depth2__list');
     $('.gnb-depth2__list').removeClass('is-active');
-    if(depth2.hasClass('is-active')){
+    if(depth1.hasClass('is-active')){
+        depth1.removeClass('is-active');
         depth2.removeClass('is-active');
     }else{
         depth2.addClass('is-active');
+        depth1.addClass('is-active').siblings().removeClass('is-active');
     }
 })
